17a1768b81e14e7f4c607bb7f0620a9ce1212b7e
Nithin Reddy Ganji <nithinreddyganji@Nithins-MacBook-Pro.local>
2018-11-01 00:08:21 -0400

    fixed minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import {HttpHeaders,HttpClient} from '@angular/common/http';
-import {Http,Headers} from '@angular/http';
-import Stomp from 'stompjs';
-import SockJS from 'sockjs-client';
-import $ from 'jquery';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { Http, Headers } from '@angular/http';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import * as $ from 'jquery';
+
+
 
 @Component({
   selector: 'app-root',
@@ -11,67 +13,80 @@ import $ from 'jquery';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  private serverUrl = 'http://localhost:8080/stomp/socket/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzY3MzcyNjAsInVzZXJfbmFtZSI6Im5pdGhpbmdhbmppIiwiYXV0aG9yaXRpZXMiOlsiVVNFUiJdLCJqdGkiOiI0NmUzMDYyZS1lMTI1LTRjZDYtYTMwMi02NWNhODNmZWNmZjUiLCJjbGllbnRfaWQiOiJjbGllbnQiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.fnMzJAGailFKh96--kZyVKwAHFqxdvytfhT-dvg1sLM';
+  private serverUrl = 'http://localhost:8080/stomp/socket/?access_token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDEwMTI4NjksInVzZXJfbmFtZSI6InRlc3QiLCJhdXRob3JpdGllcyI6WyJVU0VSIl0sImp0aSI6ImU3Y2U5YWU1LTE5NGMtNDY3OC1iZDg4LWVhNTk1ZDg5NDQyZSIsImNsaWVudF9pZCI6ImNsaWVudCIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdfQ.sqxIwdlEyDXoyOpcMR12IJxWeWzkfZOG_HPHputMWuM';
   private title = 'WebSockets chat';
   private stompClient;
-  private sessionId=null;
-  private sampleMessage={
-                        "messageSender":"sample",
-                        "messageReceiver":"nithinganji",
-                        "messageContent":"sample content"};
+  private sessionId = null;
+  private sampleMessage = {
+    "messageSender": "test",
+    "messageReceiver": "nithin",
+    "messageContent": "sample content"
+  };
 
-  private headers:HttpHeaders; 
-  
-  constructor(private http:Http){
+  private headers: HttpHeaders;
+
+  constructor(private http: Http) {
     this.initializeWebSocketConnection();
   }
 
-  initializeWebSocketConnection(){
+  initializeWebSocketConnection() {
     // this.http = new HttpClient();
     this.headers = new HttpHeaders({
       'Content-Type': 'application/json',
-      'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzY3MzcyNjAsInVzZXJfbmFtZSI6Im5pdGhpbmdhbmppIiwiYXV0aG9yaXRpZXMiOlsiVVNFUiJdLCJqdGkiOiI0NmUzMDYyZS1lMTI1LTRjZDYtYTMwMi02NWNhODNmZWNmZjUiLCJjbGllbnRfaWQiOiJjbGllbnQiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.fnMzJAGailFKh96--kZyVKwAHFqxdvytfhT-dvg1sLM'
+      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDEwMTI4NjksInVzZXJfbmFtZSI6InRlc3QiLCJhdXRob3JpdGllcyI6WyJVU0VSIl0sImp0aSI6ImU3Y2U5YWU1LTE5NGMtNDY3OC1iZDg4LWVhNTk1ZDg5NDQyZSIsImNsaWVudF9pZCI6ImNsaWVudCIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdfQ.sqxIwdlEyDXoyOpcMR12IJxWeWzkfZOG_HPHputMWuM'
     });
 
     const httpOptions = {
       headers: new Headers({
-        'Authorization':'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1MzY3MzcyNjAsInVzZXJfbmFtZSI6Im5pdGhpbmdhbmppIiwiYXV0aG9yaXRpZXMiOlsiVVNFUiJdLCJqdGkiOiI0NmUzMDYyZS1lMTI1LTRjZDYtYTMwMi02NWNhODNmZWNmZjUiLCJjbGllbnRfaWQiOiJjbGllbnQiLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXX0.fnMzJAGailFKh96--kZyVKwAHFqxdvytfhT-dvg1sLM'
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NDEwMTI4NjksInVzZXJfbmFtZSI6InRlc3QiLCJhdXRob3JpdGllcyI6WyJVU0VSIl0sImp0aSI6ImU3Y2U5YWU1LTE5NGMtNDY3OC1iZDg4LWVhNTk1ZDg5NDQyZSIsImNsaWVudF9pZCI6ImNsaWVudCIsInNjb3BlIjpbInJlYWQiLCJ3cml0ZSJdfQ.sqxIwdlEyDXoyOpcMR12IJxWeWzkfZOG_HPHputMWuM'
       })
     };
 
-    //this.http.get('http://localhost:8080/api/hello',httpOptions).subscribe(response=>console.log(response));
+    this.http.get('http://localhost:8080/api/hello', httpOptions).subscribe(response => console.log(response));
 
-     let ws = new SockJS(this.serverUrl);
+    let ws = new SockJS(this.serverUrl);
     this.stompClient = Stomp.over(ws);
     let that = this;
-    this.stompClient.connect({"message":"CONNECT"}, function(frame) {
+    this.stompClient.connect({}, function (frame) {
       //setConnected(true);
+      console.log(frame);
       var url = that.stompClient.ws._transport.url;
       console.log(that.stompClient.ws._transport.url);
       url = url
-          .replace(
-              "ws://localhost:8080/stomp/socket/",
-              "");
+        .replace(
+          "ws://localhost:8080/stomp/socket/",
+          "");
       url = url.replace("/websocket", "");
       url = url.replace(/^[0-9]+\//, "");
-      url = url.substring(0,url.indexOf("?"));
-      //console.log(url.substring(0,url.indexOf("?")));
+      url = url.substring(0, url.indexOf("?"));
+     
       console.log("Your current session is: " + url);
       that.sessionId = url;
-      that.stompClient.subscribe("/user/rchat/chat/queue"+"-user"+that.sessionId, (message) => {
-        if(message.body) {
-          $(".chat").append("<div class='message'>"+message.body+"</div>")
-          console.log(message.body);
+      that.stompClient.subscribe("/user/rchat/chat/queue" + "-user" + that.sessionId, (message) => {
+
+        if (message.body) {
+
+          $(".chat").append("<div class='message'>" + message.body + "</div>")
+
         }
       });
       //console.log(that.sampleMessage);
-      that.stompClient.send("/app/stomp/socket",{},JSON.stringify(that.sampleMessage));
+      that.stompClient.send("/app/stomp/socket", {}, JSON.stringify(that.sampleMessage));
     });
   }
-/*
-  sendMessage(message){
-    this.stompClient.send("/app/send/message" , {}, message);
+
+  sendMessage(text) {
+    console.log(text);
+    
+    const newmessage:any = {
+      
+    };
+
+    //newmessage.messageSender='test';
+    newmessage.messageReceiver='test';
+    newmessage.messageContent=text;
+    this.stompClient.send("/app/stomp/socket", {}, JSON.stringify(newmessage));
     $('#input').val('');
-  } */
-  
+  }
+
 }
